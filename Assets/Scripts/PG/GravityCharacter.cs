478ff85015f50dbba4d49ed3bcf9d9f5cd117cd1
France <frascic231@gmail.com>
2026-05-22 14:54:37 +0200

    xbfd

---
+++ Assets/Scripts/PG/GravityCharacter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCharacter : MonoBehaviour
+{
+    public float speed;
+    public float gravityForce = 9.81f; 
+    public GravityCollider gc;
+
+    private bool isGrounded = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Debug.Log(isGrounded);
+        // Se NON è sul terreno, applica la gravità verso il basso
+        if (!isGrounded)
+        {
+            transform.Translate(Vector3.down * gravityForce * Time.deltaTime);
+        }
+
+        /* Qui puoi gestire il movimento orizzontale standard con 'speed'
+        float moveX = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * moveX * speed * Time.deltaTime);
+        */
+    }
+
+    // Funzione pubblica che permette al GravityCollider di cambiare lo stato
+    public void SetGrounded(bool grounded)
+    {
+        isGrounded = grounded;
+    }
+}
---
+++ Assets/Scripts/PG/GravityCollider.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCollider : MonoBehaviour
+{
+    // Riferimento allo script principale del personaggio
+    private GravityCharacter character;
+
+    void Start()
+    {
+        
+        // Cerca lo script GravityCharacter nell'oggetto padre
+        character = GetComponentInParent<GravityCharacter>();
+    }
+
+    // Quando il trigger entra in contatto con qualcosa
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        // Controlla se l'oggetto toccato ha il tag "Ground"
+        if (collision.CompareTag("Ground"))
+        {
+            Debug.Log("si");
+            character.SetGrounded(true);
+        }
+    }
+
+    // Quando il trigger si stacca dal terreno
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            character.SetGrounded(false);
+        }
+    }
+}
